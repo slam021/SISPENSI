7daf7e5d51bab3ec94ae0485f10c00a71afeb769
slam021 <slam21.other@gmail.com>
2022-12-19 17:02:09 +0700

    update: lap fund acct

--- app/Http/Controllers/FundingAcctReportController.php
+++ app/Http/Controllers/FundingAcctReportController.php
@@ -2,9 +2,229 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\CoreCandidate;
+use App\Models\FinancialFlow;
+use App\Models\CoreTimses;
+use App\Models\FinancialCategory;
+use Elibyy\TCPDF\Facades\TCPDF;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 class FundingAcctReportController extends Controller
 {
-    //
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    public function index(){
+        if(!$start_month = Session::get('start_month')){
+            $start_month = date('m');
+        }else{
+            $start_month = Session::get('start_month');
+        }
+        if(!$end_month = Session::get('end_month')){
+            $end_month = date('m');
+        }else{
+            $end_month = Session::get('end_month');
+        }
+        if(!$year = Session::get('year')){
+            $year = date('Y');
+        }else{
+            $year = Session::get('year');
+        }
+
+        $monthlist = array(
+            '01' => 'Januari',
+            '02' => 'Februari',
+            '03' => 'Maret',
+            '04' => 'April',
+            '05' => 'Mei',
+            '06' => 'Juni',
+            '07' => 'Juli',
+            '08' => 'Agustus',
+            '09' => 'September',
+            '10' => 'Oktober',
+            '11' => 'November',
+            '12' => 'Desember',
+        );
+
+        $year_now 	=	date('Y');
+        for($i=($year_now-2); $i<($year_now+2); $i++){
+            $yearlist[$i] = $i;
+        } 
+
+        if(!Session::get('start_date')){
+            $start_date     = date('Y-m-d');
+        }else{
+            $start_date = Session::get('start_date');
+        }
+        if(!Session::get('end_date')){
+            $end_date     = date('Y-m-d');
+        }else{
+            $end_date = Session::get('end_date');
+        }
+
+        $code=[
+            '' => '',
+            '1' => 'Kandidat',
+            '2' => 'Timses',
+        ];
+
+        $listfinancialcategory = FinancialCategory::where('data_state', '=', 0)
+        ->pluck('financial_category_name', 'financial_category_id');
+
+        $listcoretimses = CoreTimses :: where('data_state', 0)
+        ->get()
+        ->pluck('timses_name', 'timses_id');
+
+        $listcorecandidate = CoreCandidate :: where('data_state', 0)
+        ->get()
+        ->pluck('candidate_full_name', 'candidate_id');
+
+        $category_income = FinancialCategory::where('financial_category.data_state', '=', 0)
+        // ->join('financial_flow', 'financial_category.financial_category_id', '=', 'financial_flow.financial_category_id')
+        // ->whereMonth('financial_flow.financial_flow_date', $start_month)
+        // ->whereYear('financial_flow.financial_flow_date',$year)
+        // ->orderBy('financial_flow.financial_flow_date', 'DESC')
+        // ->orderBy('financial_flow.last_balance_candidate', 'DESC')
+        ->where('financial_category.financial_category_type', '=', 1)
+        ->get();
+
+        $category_expenditure = FinancialCategory::where('financial_category.data_state', '=', 0)
+        // ->join('financial_flow', 'financial_category.financial_category_id', '=', 'financial_flow.financial_category_id')
+        // ->whereMonth('financial_flow.financial_flow_date', $start_month)
+        // ->whereYear('financial_flow.financial_flow_date',$year)
+        // ->orderBy('financial_flow.financial_flow_date', 'DESC')
+        // ->orderBy('financial_flow.last_balance_candidate', 'DESC')
+        ->where('financial_category.financial_category_type', '=', 2)
+        ->get();
+
+        $last_balance_timses_old = FinancialFlow::where('financial_flow.data_state', '=', 0)
+        ->whereMonth('financial_flow.financial_flow_date', $start_month)
+        ->whereYear('financial_flow.financial_flow_date',$year)
+        ->orderBy('financial_flow.financial_flow_date', 'DESC')
+        ->orderBy('financial_flow.last_balance_timses', 'DESC')
+        ->get();
+
+        $financialflow_income = FinancialFlow::where('financial_flow.data_state', '=', 0)
+        ->whereMonth('financial_flow.financial_flow_date','>=',$start_month)
+        ->whereMonth('financial_flow.financial_flow_date','<=',$end_month)
+        ->whereYear('financial_flow.financial_flow_date',$year)
+        ->where('financial_flow.financial_category_id', $category_income);
+
+        $financial_category_id = Session::get('financial_category_id');
+        $candidate_id = Session::get('candidate_id');
+        $timses_id = Session::get('timses_id');
+        $financialflow_list = Session::get('financialflow_list');
+        
+        if($financial_category_id||$financial_category_id!=null||$financial_category_id!=''){
+            $financialflow_income   = $financialflow_income->where('financial_category_id', $financial_category_id);
+        }
+
+        if($financialflow_list||$financialflow_list!=null||$financialflow_list!=''){
+            if($financialflow_list == 1){           
+                $financialflow_income   = $financialflow_income->where('candidate_id', '!=', null);
+            }else{
+                $financialflow_income   = $financialflow_income->where('timses_id', '!=', null);
+            }
+        }else{
+            $financialfinancialflow_incomeflow   = $financialflow_income->where('candidate_id', '=', null);
+            $financialflow_income   = $financialflow_income->where('timses_id', '=', null);
+        }
+
+        $financialflow_income   = $financialflow_income->get();
+        
+        $financialflow = FinancialFlow::where('financial_flow.data_state', '=', 0)
+        ->whereMonth('financial_flow.financial_flow_date','>=',$start_month)
+        ->whereMonth('financial_flow.financial_flow_date','<=',$end_month)
+        ->whereYear('financial_flow.financial_flow_date',$year)
+        ->where('financial_flow.financial_category_id', $category_expenditure);
+
+        $financial_category_id = Session::get('financial_category_id');
+        $candidate_id = Session::get('candidate_id');
+        $timses_id = Session::get('timses_id');
+        $financialflow_list = Session::get('financialflow_list');
+        
+        if($financial_category_id||$financial_category_id!=null||$financial_category_id!=''){
+            $financialflow   = $financialflow->where('financial_category_id', $financial_category_id);
+        }
+
+        if($financialflow_list||$financialflow_list!=null||$financialflow_list!=''){
+            if($financialflow_list == 1){           
+                $financialflow   = $financialflow->where('candidate_id', '!=', null);
+            }else{
+                $financialflow   = $financialflow->where('timses_id', '!=', null);
+            }
+        }else{
+            $financialflow   = $financialflow->where('candidate_id', '=', null);
+            $financialflow   = $financialflow->where('timses_id', '=', null);
+        }
+
+        $financialflow   = $financialflow->get();
+
+        return view('content.FundingAcctReport_view.ReportFundingAcct', compact('start_month','end_month', 'monthlist', 'year' , 'yearlist', 'year_now', 'listfinancialcategory', 'financial_category_id', 'timses_id', 'candidate_id', 'listcoretimses', 'listcorecandidate', 'financialflow', 'start_date', 'end_date', 'financialflow_list', 'category_income', 'category_expenditure', 'last_balance_timses_old', 'code', 'financialflow_income'));
+    }
+
+    public function filterFundingAcctReport(Request $request){
+        $start_month  = $request->start_month;
+        $end_month    = $request->end_month;
+        $start_date  = $request->start_date;
+        $end_date    = $request->end_date;
+        $year         = $request->year;
+        $timses_id    = $request->timses_id;
+        $candidate_id = $request->candidate_id;
+        $financial_category_id = $request->financial_category_id;
+        $financialflow_list = $request->financialflow_list;
+
+        Session::put('timses_id', $timses_id);
+        Session::put('candidate_id', $candidate_id);
+        Session::put('financial_category_id', $financial_category_id);
+        Session::put('financialflow_list', $financialflow_list);
+        Session::put('start_month',$start_month);
+        Session::put('end_month',$end_month);
+        Session::put('start_date',$start_date);
+        Session::put('end_date',$end_date);
+        Session::put('year',$year);
+// dd($financialflow_list);
+        return redirect('/report-funding');
+    }
+
+    public function filterResetFundingAcctReport(){
+        Session::put('start_month');
+        Session::put('end_month');
+        Session::put('start_date');
+        Session::put('end_date');
+        Session::put('year');
+        Session::put('year');
+        Session::forget('timses_id');
+        Session::forget('candidate_id');
+        Session::forget('financial_category_id');
+        Session::forget('financialflow_list');
+
+        return redirect('/report-funding');
+    }
+
+    public function getCategoryName($financial_category_id){
+        $data = FinancialCategory::where('financial_category_id',$financial_category_id)
+        ->first();
+
+        return $data['financial_category_name'];
+    }
+
+    public function getTimsesName($timses_id){
+        $data = CoreTimses::where('timses_id', $timses_id)
+        ->first();
+
+        return $data['timses_name'];
+    }
+
+    public function getCandidateName($candidate_id){
+        $data = CoreCandidate::where('candidate_id',$candidate_id)
+        ->first();
+
+        return $data['candidate_full_name'];
+    }
 }
---
+++ resources/views/content/FundingAcctReport_view/ReportFundingAcct.blade.php
@@ -0,0 +1,292 @@
+@inject('RFA','App\Http\Controllers\FundingAcctReportController')
+@extends('adminlte::page')
+
+@section('title', 'Sistem Pendukung Eleksi')
+<link rel="shortcut icon" href="{{ asset('resources/assets/logo_vote.ico') }}" />
+
+@section('js')
+<script>
+    $(document).ready(function(){
+        var financial_category_id = {!! json_encode($financial_category_id) !!};
+        
+        if(financial_category_id == null){
+            $("#financial_category_id").select2("val", "0");
+        }
+    });
+    $(document).ready(function(){
+        var candidate_id = {!! json_encode($candidate_id) !!};
+        
+        if(candidate_id == null){
+            $("#candidate_id").select2("val", "0");
+        }
+    });
+    $(document).ready(function(){
+        var timses_id = {!! json_encode($timses_id) !!};
+        
+        if(timses_id == null){
+            $("#timses_id").select2("val", "0");
+        }
+    });
+</script>
+@stop
+
+@section('content_header')
+    
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ url('home') }}">Beranda</a></li>
+        <li class="breadcrumb-item active" aria-current="page"> Daftar Laporan Perhitungan Keuangan </li>
+    </ol>
+</nav>
+
+@stop
+
+@section('content')
+<?php
+    function rupiah($angka){
+                        $hasil_rupiah = "Rp. " . number_format($angka,2,',','.');
+                        return $hasil_rupiah;
+                    } 
+?>
+{{-- <h3 class="page-title">
+    <b>Daftar Buku Besar </b> <small>Kelola Daftar Buku Besar  </small>
+</h3> --}}
+<div id="accordion">
+    <form  method="post" action="{{ route('filter-report-funding') }}" enctype="multipart/form-data">
+        @csrf
+            <div class="card border border-dark">
+            <div class="card-header bg-dark" id="headingOne" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <h5 class="mb-0">
+                    Filter
+                </h5>
+                {{-- <div class="form-actions float-right">
+                    <button onclick="location.href='{{ url('funding-income-timses/add') }}'" name="add" class="btn btn-sm bg-cyan" title="Add Data"><i class="fa fa-plus"></i> Kembali</button>
+                </div> --}}
+            </div>
+        
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="card-body">
+                    <div class = "row">
+                        <div class = "col-md-4">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Periode Awal
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                {!! Form::select(0, $monthlist, $start_month, ['class' => 'selection-search-clear select-form', 'id' => 'start_month', 'name' => 'start_month']) !!}
+                            </div>
+                        </div>
+    
+                        <div class = "col-md-4">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Periode Akhir
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                {!! Form::select(0, $monthlist, $end_month, ['class' => 'selection-search-clear select-form', 'id' => 'end_month', 'name' => 'end_month']) !!}
+                            </div>
+                        </div>
+                        <div class = "col-md-4">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Tahun
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                {!! Form::select(0, $yearlist, $year, ['class' => 'selection-search-clear select-form', 'id' => 'year', 'name' => 'year']) !!}
+                            </div>
+                        </div>
+
+                        {{-- <div class = "col-md-3">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Tanggal Mulai
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                <input type="month" class="form-control input-bb" id="start_month" name="start_month" value="{{ $start_month }}">
+                            </div>
+                        </div>
+    
+                        <div class = "col-md-3">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Tanggal Akhir
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                <input type="month" class="form-control input-bb"  id="end_month" name="end_month" value="{{ $end_month }}">
+                            </div>
+                        </div> --}}
+                        <div class = "col-md-4">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Kepemilikan
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                {!! Form::select('financialflow_list', $code, $financialflow_list, ['class' => 'selection-search-clear select-form', 'id' => 'financialflow_list' ])!!}
+                            </div>
+                        </div>
+                        {{-- <div class = "col-md-6">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Kategori
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                {!! Form::select('financial_category_id', $listfinancialcategory, $financial_category_id, ['class' => 'selection-search-clear select-form', 'id' => 'financial_category_id','' ])!!}
+                            </div>
+                        </div> --}}
+                        {{-- <div class = "col-md-4">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Nama Kandidat
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                {!! Form::select('candidate_id', $listcorecandidate, $candidate_id, ['class' => 'selection-search-clear select-form', 'id' => 'candidate_id','' ])!!}
+                            </div>
+                        </div>
+                        <div class = "col-md-4">
+                            <div class="form-group form-md-line-input">
+                                <section class="control-label">Nama Timses
+                                    <span class="required text-danger">
+                                        *
+                                    </span>
+                                </section>
+                                {!! Form::select('timses_id', $listcoretimses, $timses_id, ['class' => 'selection-search-clear select-form', 'id' => 'timses_id','' ])!!}
+                            </div>
+                        </div> --}}
+                    </div>
+                </div>
+                <div class="card-footer text-muted">
+                    <div class="form-actions float-right">
+                        <a href="{{ route('filter-reset-report-funding') }}" type="button" name="Reset" class="btn bg-yellow btn-sm"><i class="fas fa-sync"></i> Reset</a>
+                        <button type="submit" name="Find" class="btn btn-primary btn-sm" title="Search Data"><i class="fa fa-search"></i> Cari</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </form>
+</div>
+@if(session('msg'))
+<div class="alert alert-info" role="alert">
+    {{session('msg')}}
+</div>
+@endif 
+<div class="card border border-dark">
+    <div class="card-header bg-dark clearfix">
+        <h5 class="mb-0 float-left">
+            Daftar
+        </h5>
+    </div>
+
+    <div class="container"> 
+    <div class="card-body">
+        <div class="table-responsive pt-3">
+            <table id="" style="width:100%" class="table-sm table-bordered table-hover" class="table table-hover table-bordered table-full-width table-responsive-sm">
+                <thead>
+                    <tr>
+                        <td colspan='2' style='text-align:center;'>
+                            <div style='font-weight:bold'>Laporan Perhitungan Keuangan {{$code[$financialflow_list]}}
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan='2' style='text-align:center;'>
+                            <div style='font-weight:bold'>
+                                Periode {{ $monthlist[$start_month] }} s.d. {{  $monthlist[$end_month] }}  {{ $year }}
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan='2'></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan='2' style='font-weight:bold'>Rincian Pemasukan</td>
+                    </tr>
+                    <tr>
+                        <td colspan='2' style='font-weight:bold; padding-right:1px'> &emsp;&emsp;Kategori Keuangan</td>
+                    </tr>
+                    @foreach ($category_income as $val)
+                            <tr>
+                                <td colspan=''>&emsp;&emsp;&emsp;&emsp;{{$val['financial_category_name']}}</td>
+                                @foreach($financialflow_income as $nominal)
+                      
+                        
+                                <td style='text-align:right;'>{{$nominal}}</td>
+                
+                              
+                            @endforeach
+                            </tr>
+
+                    @endforeach
+                    <tr>
+                        <td style='font-weight:bold'>Total Pemasukan</td>
+                        <td style='font-weight:bold; text-align:right;'>0,00</td>
+                    </tr>
+                    <tr>
+                        <td colspan='2'></td>
+                    </tr>
+                    <tr>
+                        <td colspan='2' style='font-weight:bold'>Rincian Pengeluaran</td>
+                    </tr>
+                    <tr>
+                        <td colspan='2' style='font-weight:bold'>&emsp;&emsp;Kategori Keuangan</td>
+                    </tr>
+                    @foreach ($category_expenditure as $val)
+                    <tr>
+                        <td>&emsp;&emsp;&emsp;&emsp;{{$val['financial_category_name']}}</td>    
+                        @foreach($financialflow as $nominal)
+                      
+                        
+                                <td style='text-align:right;'>{{rupiah($val['financial_category_id'][$nominal['financial_flow_nominal']])}}</td>
+                
+                              
+                            @endforeach
+                    </tr>
+                    @endforeach
+                    <tr>
+                        <td style='font-weight:bold'>Total Pengeluaran</td>
+                        <td style='font-weight:bold; text-align:right;'>0,00</td>
+                    </tr>
+                    <tr>
+                        <td colspan='2'></td>
+                    </tr>
+                    <tr>
+                        <td style="font-weight:bold; width: 80%">Sisa Saldo</td>
+                        <td style='font-weight:bold; text-align:right;'>0,00</td>
+                        {{-- <th style="width: 20%; text-align: right">{{ number_format($grand_total_account_amount1 - $grand_total_account_amount2,2,'.',',') }}</th> --}}
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </div>
+    <div class="card-footer text-muted">
+        <div class="form-actions float-right">
+            <a class="btn bg-orange btn-sm" href="{{ url('/report-recap/print') }}"><i class="fa fa-file-pdf"></i> Pdf</a>
+            <a class="btn bg-olive btn-sm" href="{{ url('/report-recap/export') }}"><i class="fa fa-download"></i> Export Data</a>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+@stop
+
+@section('footer')
+    
+@stop
+
+@section('css')
+    
+@stop
+
+@section('js')
+    
+@stop   
